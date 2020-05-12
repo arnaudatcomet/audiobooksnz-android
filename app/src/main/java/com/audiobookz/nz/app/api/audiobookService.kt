811f91d63fc34e1249963502bf2f0ac60f938874
xok-comet <xok@comet.la>
2020-05-12 14:55:08 +0700

    add service for category endpoint

--- app/src/main/java/com/audiobookz/nz/app/api/audiobookService.kt
+++ app/src/main/java/com/audiobookz/nz/app/api/audiobookService.kt
@@ -4,6 +4,8 @@ package com.audiobookz.nz.app.api
 //import com.elifox.legocatalog.legoset.data.LegoSet
 //import com.elifox.legocatalog.legotheme.data.LegoTheme
 
+import com.audiobookz.nz.app.browse.data.Category
+import com.elifox.legocatalog.api.ResultsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -17,12 +19,15 @@ interface AudiobookService {
     companion object {
         const val ENDPOINT = "https://audiobooksnz.co.nz/backend/v1/"
     }
-//
-//    @GET("lego/themes/")
-//    suspend fun getThemes(@Query("page") page: Int? = null,
-//                  @Query("page_size") pageSize: Int? = null,
-//                  @Query("ordering") order: String? = null): Response<ResultsResponse<LegoTheme>>
-//
+
+    @GET("categories/")
+    suspend fun getCategory(
+        @Query("expand") expand: String? = null,
+        @Query("page") page: Int? = null,
+        @Query("per-page") pageSize: Int? = null,
+        @Query("filter[parent_id]") filter: Int? = 0
+    ): Response<ResultsResponse<Category>>
+
 //    @GET("lego/sets/")
 //    suspend fun getSets(@Query("page") page: Int? = null,
 //                        @Query("page_size") pageSize: Int? = null,
