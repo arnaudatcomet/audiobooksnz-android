6a69c090ae0fb04ec759e6ac070974436821276e
xok-comet <xok@comet.la>
2020-05-21 17:06:46 +0700

    fix email login logic

--- app/src/main/java/com/audiobookz/nz/app/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/audiobookz/nz/app/login/ui/LoginViewModel.kt
@@ -1,14 +1,16 @@
 package com.audiobookz.nz.app.login.ui
-
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
-import com.audiobookz.nz.app.audiobookList.data.AudiobookListRepository
+import com.audiobookz.nz.app.data.Result
 import com.audiobookz.nz.app.login.data.LoginRepository
+import com.audiobookz.nz.app.login.data.UserData
 import javax.inject.Inject
 
-
-class LoginViewModel @Inject constructor(repository: LoginRepository) : ViewModel() {
-    // Need to inject the data we want
-    lateinit var Username: String
-    lateinit var Password: String
-    val loginEmail by lazy {   repository.loginEmail(Username, Password)}
+class LoginViewModel @Inject constructor(private val repository: LoginRepository) : ViewModel() {
+    var logInResult = MediatorLiveData<Result<UserData>>()
+    fun loginEmail(username:String,password:String) {
+        logInResult.addSource(repository.loginEmail(username, password)){value->
+            logInResult.value = value
+        }
+    }
 }
