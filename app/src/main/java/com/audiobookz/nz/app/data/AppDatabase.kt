1a4f2fab700906f52afa4b1f12ea95b7d42001c4
xok-comet <xok@comet.la>
2020-05-22 13:05:02 +0700

    fix local database version

--- app/src/main/java/com/audiobookz/nz/app/data/AppDatabase.kt
+++ app/src/main/java/com/audiobookz/nz/app/data/AppDatabase.kt
@@ -20,7 +20,7 @@ import com.audiobookz.nz.app.worker.SeedDatabaseWorker
  * The Room database for this app
  */
 @Database(entities = [Category::class,AudiobookList::class,UserData::class],
-        version = 8, exportSchema = false)
+        version = 9, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
