ace134624a0ec221077022cc0fe439ad52e4a87f
Jo-comet <sivixay@comet.la>
2020-05-25 16:18:30 +0700

    addBodyForEditprofile

--- app/src/main/java/com/audiobookz/nz/app/profile/ui/EditProfileViewModel.kt
+++ app/src/main/java/com/audiobookz/nz/app/profile/ui/EditProfileViewModel.kt
@@ -16,7 +16,7 @@ import javax.inject.Inject
 class EditProfileViewModel  @Inject constructor(private val repository: ProfileRepository): ViewModel(){
     var editProfileResult = MediatorLiveData<Result<UserData>>()
 
-    fun editProfile(Token:String,Image:String) {
+    fun editProfile(Token:String,Image:String,firstname:String, lastname:String, oldPassword:String, newPassword:String, confirmPassword:String) {
 
 
         val file = File(Image)
@@ -27,6 +27,11 @@ class EditProfileViewModel  @Inject constructor(private val repository: ProfileR
         )
         val body =
             MultipartBody.Part.createFormData("imgFile", file.name, fbody)
+            MultipartBody.Part.createFormData("first_name", firstname)
+            MultipartBody.Part.createFormData("last_name", lastname)
+            MultipartBody.Part.createFormData("oldPassword",oldPassword)
+            MultipartBody.Part.createFormData("newPassword", newPassword)
+            MultipartBody.Part.createFormData("confirmPassword", confirmPassword)
 
 
         editProfileResult.addSource(repository.editProfile(Token, body)){value->
